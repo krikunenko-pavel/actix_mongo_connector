4dbe1a40535490d875c32feb3a545faa9757eaf0
pavelkrikunenko <pavelkrikunenko@yandex.ru>
2024-05-24 19:07:20 +0300

    new

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+use actix::{Actor, Context};
+use mongodb::Client;
+
+pub struct MongoActor{
+    pub client: Client
+}
+
+impl MongoActor{
+    pub async fn new(connection_url: String) ->Self{
+        Self{
+            client: Client::with_uri_str(
+                connection_url
+            ).await.expect("Can't create mongo client")
+        }
+
+    }
+}
+
+
+impl Actor for MongoActor{
+    type Context = Context<Self>;
+}
